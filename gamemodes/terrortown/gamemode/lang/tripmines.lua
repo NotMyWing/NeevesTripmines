e66291ef2ac08b1fdfcfde910acc6e931c55ff9d
Neeve <winwyv@gmail.com>
2018-10-16 15:10:27 +1100

    Initial commit

---
+++ gamemodes/terrortown/gamemode/lang/tripmines.lua
@@ -0,0 +1,11 @@
+local L = LANG.CreateLanguage("English")
+L.tripmine_name = "Trip Mine"
+L.tripmine_desc = "Can be planted on any static surface.\nEmits a slight red laser."
+L.tripmine_broken = "Your tripmine has been destroyed!"
+L.tripmine_help_plant = "Press the attack key to plant the mine"
+
+L = LANG.CreateLanguage("Русский")
+L.tripmine_name = "Лазерная Мина"
+L.tripmine_desc = "Можно поставить на любую статичную поверхность.\nИспускает прозрачный красный лазер."
+L.tripmine_broken = "Вашу лазерную мину уничтожили!"
+L.tripmine_help_plant = "Нажмите кнопку атаки для установки мины"
